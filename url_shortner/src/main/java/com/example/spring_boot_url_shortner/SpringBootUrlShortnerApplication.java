0b40fd770d4f510f003858776f26b082a1060277
Kunal Krishna <kunalkrishna85@gmail.com>
2026-02-17 01:18:29 -0500

    renamed artifact

---
+++ url_shortner/src/main/java/com/example/spring_boot_url_shortner/SpringBootUrlShortnerApplication.java
@@ -0,0 +1,20 @@
+package com.example.spring_boot_url_shortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+//@Configuration
+//@ComponentScan
+//@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+public class SpringBootUrlShortnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUrlShortnerApplication.class, args);
+	}
+
+}
